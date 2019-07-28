0cc59121a56e262e01009d7b8e8a548be49e7d66
Jeff Heaton <jeff@jeffheaton.com>
2019-07-27 19:59:42 -0500

    Added web page example.

---
+++ py/image_web_server_1.py
@@ -0,0 +1,82 @@
+# T81-558: Applications of Deep Neural Networks
+# Module 13: Advanced/Other Topics
+# Instructor: [Jeff Heaton](https://sites.wustl.edu/jeffheaton/), McKelvey School of Engineering, [Washington University in St. Louis](https://engineering.wustl.edu/Programs/Pages/default.aspx)
+# For more information visit the [class website](https://sites.wustl.edu/jeffheaton/t81-558/).
+# Deploy simple Keras tabular model with Flask only.
+from flask import Flask, request, jsonify,send_from_directory
+import uuid
+import os
+from tensorflow.keras.models import load_model
+import numpy as np
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from tensorflow.keras.applications import MobileNet
+from PIL import Image, ImageFile
+from io import BytesIO
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.applications.mobilenet import decode_predictions
+
+UPLOAD_FOLDER = '/Users/jheaton/test/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+IMAGE_CHANNELS = 3
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+model = MobileNet(weights='imagenet',include_top=True)
+
+@app.route('/', methods=['GET'])
+def send_index():
+    return send_from_directory('./www', "index.html")
+
+@app.route('/<path:path>', methods=['GET'])
+def send_root(path):
+    return send_from_directory('./www', path)
+
+@app.route('/api/image', methods=['POST'])
+def upload_image():
+  # check if the post request has the file part
+  if 'image' not in request.files:
+      return jsonify({'error':'No posted image. Should be attribute named image.'})
+  file = request.files['image']
+
+  # if user does not select file, browser also
+  # submit a empty part without filename
+  if file.filename == '':
+      return jsonify({'error':'Empty filename submitted.'})
+  if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      x = []
+      ImageFile.LOAD_TRUNCATED_IMAGES = False
+      img = Image.open(BytesIO(file.read()))
+      img.load()
+      img = img.resize((IMAGE_WIDTH,IMAGE_HEIGHT),Image.ANTIALIAS)    
+      x = image.img_to_array(img)
+      x = np.expand_dims(x, axis=0)
+      x = preprocess_input(x)
+      x = x[:,:,:,0:3]
+      pred = model.predict(x)
+      lst = decode_predictions(pred, top=5)
+      
+      items = []
+      for itm in lst[0]:
+        items.append({'name':itm[1],'prob':float(itm[2])})
+
+      response = {'pred':items}
+      print(response)
+      return jsonify(response)
+  else:
+      return jsonify({'error':'File has invalid extension'})
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
---
+++ py/www/style.css
@@ -0,0 +1,47 @@
+h3 {
+  margin-left: 15px;
+}
+.fileInput {
+  border-bottom: 4px solid #d3d3d3;
+  border-right: 4px solid #d3d3d3;
+  border-top: 1px solid #000;
+  border-left: 1px solid #000;
+  padding: 10px;
+  margin: 15px;
+  cursor: pointer;
+}
+.imgPreview {
+  text-align: center;
+  margin: 5px 15px;
+  height: 200px;
+  width: 500px;
+  border-left: 1px solid #808080;
+  border-right: 1px solid #808080;
+  border-top: 5px solid #808080;
+  border-bottom: 5px solid #808080;
+}
+.imgPreview img {
+  width: 100%;
+  height: 100%;
+}
+.previewText {
+  width: 100%;
+  margin-top: 20px;
+}
+.submitButton {
+  padding: 12px;
+  margin-left: 10px;
+  background: #fff;
+  border: 4px solid #d3d3d3;
+  border-radius: 15px;
+  font-weight: 700;
+  font-size: 10pt;
+  cursor: pointer;
+}
+.submitButton:hover {
+  background: #efefef;
+}
+.centerText {
+  text-align: center;
+  width: 500px;
+}
